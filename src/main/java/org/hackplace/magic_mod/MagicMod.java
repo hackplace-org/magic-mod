2ef5444eab504e2741b88e394d7250abd11fbe4b
Anish Pallati <anishp@duck.com>
2023-08-16 15:35:48 -0400

    Initial commit

---
+++ src/main/java/org/hackplace/magic_mod/MagicMod.java
@@ -0,0 +1,16 @@
+package org.hackplace.magic_mod;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MagicMod implements ModInitializer {
+	public static final String MOD_ID = "magic-mod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		LOGGER.info("Hello Fabric world!");
+	}
+}
---
+++ src/main/java/org/hackplace/magic_mod/MagicModClient.java
@@ -0,0 +1,10 @@
+package org.hackplace.magic_mod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MagicModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
