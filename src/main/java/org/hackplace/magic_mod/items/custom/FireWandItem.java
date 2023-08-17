106bf8ce34a7a411a529343fbf0ea0dfe2713bca
Anish Pallati <anishp@duck.com>
2023-08-17 12:23:00 -0400

    feat: Add FireWandItem to thematic magic wands

--- src/main/java/org/hackplace/magic_mod/items/custom/FireWandItem.java
+++ src/main/java/org/hackplace/magic_mod/items/custom/FireWandItem.java
@@ -1,2 +1,23 @@
-package org.hackplace.magic_mod.items.custom;public class FireWandItem {
+package org.hackplace.magic_mod.items.custom;
+
+import net.minecraft.client.item.TooltipContext;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.text.Text;
+import net.minecraft.world.World;
+import org.hackplace.magic_mod.MagicMod;
+
+import java.util.List;
+
+public class FireWandItem extends Item {
+    public static final String NAME = "fire_wand";
+
+    public FireWandItem(Settings settings) {
+        super(settings);
+    }
+
+    @Override
+    public void appendTooltip(ItemStack itemStack, World world, List<Text> tooltip, TooltipContext tooltipContext) {
+        tooltip.add(Text.translatable(String.format("item.%s.%s.tooltip", MagicMod.MOD_ID, NAME)));
+    }
 }
